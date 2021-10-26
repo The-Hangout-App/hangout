52085384ab1a8836eb1f0fba0ca5eee2d7509e99
ZechWolf <zechw@smu.edu>
2021-10-26 18:48:45 -0500

    installed ui package

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { ThemeConsumer, ThemeProvider } from 'react-native-elements';
+import { Header } from 'react-native-elements/dist/header/Header';
+import Login from './screens/Login';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ThemeProvider>
+      <Header placement="left"
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'Hangout', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+    </ThemeProvider>
+      
+      
     </View>
   );
 }
@@ -17,5 +27,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputs: {
+    width: "90%",
+    height: 50,
+    borderColor: "black",
+    borderWidth: 2
   },
 });
