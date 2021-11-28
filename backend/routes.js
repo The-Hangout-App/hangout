64ec5c5c9e6f4d9781acaa73d575affa436f983d
Jack Davenport <jackd2018@gmail.com>
2021-11-28 17:28:29 -0600

    login

--- backend/routes.js
+++ backend/routes.js
@@ -9,11 +9,11 @@ module.exports = function routes(app, logger) {
       var username = request.body.username;
       var password = request.body.password;
       if (username && password) {
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+        connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
           if (results.length > 0) {
             request.session.loggedin = true;
             request.session.username = username;
-            response.redirect('/home');
+            response.send('Correct Password');
           } else {
             response.send('Incorrect Username and/or Password!');
           }			
