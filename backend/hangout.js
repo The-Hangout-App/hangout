b530f4097b973ed56c2860bb0706f14573fc7f08
ZechWolf <zechw@smu.edu>
2022-04-16 20:39:56 -0500

    cert

--- backend/hangout.js
+++ backend/hangout.js
@@ -33,7 +33,7 @@ const createUnixSocketPool = async (config) => {
     // If connecting via unix domain socket, specify the path
     socketPath: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`,
     ssl: {
-      sslmode: 'verify-full',
+      //sslmode: 'verify-full',
       ca: fs.readFileSync(process.env.DB_ROOT_CERT), // e.g., '/path/to/my/server-ca.pem'
       key: fs.readFileSync(process.env.DB_KEY), // e.g. '/path/to/my/client-key.pem'
       cert: fs.readFileSync(process.env.DB_CERT), // e.g. '/path/to/my/client-cert.pem'
